b83c478f71289899cda45eff5daccca894b7dfe4
benita sien <benitasien@gmail.com>
2023-01-09 00:14:28 +1100

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import TopMenu from "./TopMenu";
+import SideMenu from "./SideMenu";
+
+import MainContent from "./MainContent";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+    <div className="app-container">
+      <SideMenu/>
+        
+      <TopMenu/>
+      <MainContent/>
+    </div>
+  );
 }
 
 export default App;
